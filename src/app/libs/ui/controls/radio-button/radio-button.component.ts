126a017d63ab236d3bc611de3633addff6e5edac
Dzmitry Bulauka <self-walking_cat@mail.ru>
2024-04-16 15:37:28 +0200

    master: add new code

---
+++ src/app/libs/ui/controls/radio-button/radio-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './radio-button.component.html',
+  styleUrl: './radio-button.component.scss'
+})
+export class RadioButtonComponent {
+
+}
